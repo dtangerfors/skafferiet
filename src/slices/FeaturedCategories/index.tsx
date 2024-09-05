ce95eaa2eace7b6a7069bffa7d6d98c341dfac92
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2024-09-05 22:07:09 +0200

    Fixed type error

--- src/slices/FeaturedCategories/index.tsx
+++ src/slices/FeaturedCategories/index.tsx
@@ -6,6 +6,7 @@ import { InnerSection, Section } from "@/app/ui/layout/containers";
 import { Heading2 } from "@/app/ui/typography";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { TagDocument } from "../../../prismicio-types";
 
 /**
  * Props for `FeaturedCategories`.
@@ -29,7 +30,7 @@ const FeaturedCategories = ({
         <Heading2>{slice.primary.title}</Heading2>
       </div>
       <div className={clsx(isMobile ? "flex gap-3 col-span-2 whitespace-nowrap overflow-auto snap-x snap-mandatory -mx-6 px-6 scrollbar-hide" : "grid grid-cols-2 md:grid-cols-3 gap-3 lg:gap-6", "mt-6")}>
-         {slice.items.map(item => <CategoryCard key={item.tag.link_type} category={item.tag} />)}
+         {slice.items.map(item => <CategoryCard key={item.tag.link_type} category={item.tag as unknown as TagDocument} />)}
       </div>
     </InnerSection>
    </Section>
